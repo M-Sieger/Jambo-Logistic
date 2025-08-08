fb7a281e3729ad520e280e20182693038d01f26b
Moritz Sieger <deine@mail.de>
2025-08-09 01:54:54 +0200

    chore: setup design foundation with tokens, base styles, and font loading optimization

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+// oben bei den Imports
+import './styles/base.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
